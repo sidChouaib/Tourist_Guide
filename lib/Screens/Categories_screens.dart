83e45cedd03d0b5e604c39a71d20fa524cad0725
Chorora <sidchouaib01@gmail.com>
2022-10-23 22:08:17 +0100

    Initial commit

---
+++ lib/Screens/Categories_screens.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tourist\'s Guide'),
+      ),
+      body: GridView(
+        gridDelegate : SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 7 / 8,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+        )
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
